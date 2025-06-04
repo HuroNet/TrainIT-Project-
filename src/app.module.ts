730026ffab4c370357e5a82017a1aadcdc797531
Huron <charlie.walls97@gmail.com>
2025-06-04 16:23:42 -0500

    implementacion del servicio de Departamento

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmpleadoModule } from './empleado/empleado.module';
 import { DepartamentoModule } from './departamento/departamento.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [EmpleadoModule, DepartamentoModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- src/departamento/departamento.service.ts
+++ src/departamento/departamento.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class DepartamentoService {}
+export class DepartamentoService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(data: { nombre: string }) {
+    return this.prisma.departamento.create({
+      data,
+    });
+  }
+
+  findAll() {
+    return this.prisma.departamento.findMany({
+      include: { empleados: true },
+    });
+  }
+
+  findOne(id: number) {
+    return this.prisma.departamento.findUnique({
+      where: { id },
+      include: { empleados: true },
+    });
+  }
+
+  update(id: number, data: { nombre: string }) {
+    return this.prisma.departamento.update({
+      where: { id },
+      data,
+    });
+  }
+
+  remove(id: number) {
+    return this.prisma.departamento.delete({
+      where: { id },
+    });
+  }
+}
